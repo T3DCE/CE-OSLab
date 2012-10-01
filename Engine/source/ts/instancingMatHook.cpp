e92dcfdc2481485eb8448ef3054a50aa205c7512
SilentMike <silent.mike.hall@gmail.com>
2012-10-01 06:07:56 -0400

    Useful TextureTarget fix:  http://www.garagegames.com/community/blogs/view/21019
    This is not the full Resource as seen in the link, only the fix to make assignment of texture targets compatible with mesh instancing.

--- Engine/source/ts/instancingMatHook.cpp
+++ Engine/source/ts/instancingMatHook.cpp
@@ -57,8 +57,14 @@ BaseMatInstance* InstancingMaterialHook::getInstancingMat( BaseMatInstance *matI
       FeatureSet features( matInst->getRequestedFeatures() );
       features.addFeature( MFT_UseInstancing ); 
 
+      // TextureTarget Fix - see http://www.garagegames.com/community/blogs/view/21019
+      Material::sAllowTextureTargetAssignment = true;
+
       if ( !instMat->init( features, matInst->getVertexFormat() ) )
          SAFE_DELETE( instMat );
+
+      // TextureTarget Fix - see http://www.garagegames.com/community/blogs/view/21019
+      Material::sAllowTextureTargetAssignment = false;
 
       hook->mMatInst = instMat;
    }
